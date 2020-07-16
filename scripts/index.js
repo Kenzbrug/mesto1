a5d1704ab7b097599fb8fad44a039cce29d5c0d9
Victor Zhabolenko <kenzbrug1337@yandex.ru>
2020-07-16 15:09:36 +0300

    fix: исправить недоработки отмеченные ревьюером

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
-const openPopupButton = document.querySelector('.profile__button');
+const openPopupButton = document.querySelector('.profile__button-change');
 const closePopupButton = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
-const formElement = popup.querySelector('.form');
-let nameInput = document.querySelector('.form__input_type_name');
-let jobInput = document.querySelector('.form__input_type_occupation');
+const formElement = popup.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form-input_type_name');
+let jobInput = document.querySelector('.popup__form-input_type_occupation');
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
 
-function togglePopup () {
-    popup.classList.toggle('popup__open');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileOccupation.textContent;
+function closePopup () {
+    popup.classList.remove('popup_open');
 }
-
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileOccupation.textContent = jobInput.value;
-    togglePopup ();
+    closePopup ();
 }
 
+openPopupButton.addEventListener('click', () => {
+    popup.classList.add ('popup_open');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileOccupation.textContent;
+})
+
+closePopupButton.addEventListener('click', closePopup)
 formElement.addEventListener('submit', formSubmitHandler);
